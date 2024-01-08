b5a58b8c95a651650b16d6695d84821c47918e64
PMinn <40393799+PMinn@users.noreply.github.com>
2024-01-09 00:30:47 +0800

    Update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -27,8 +27,6 @@ export default function MyApp({ Component, pageProps }) {
           <meta name="theme-color" content="#000000" />
           <title>會員禮計算機</title>
 
-
-
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
@@ -41,6 +39,7 @@ export default function MyApp({ Component, pageProps }) {
           <meta name="msapplication-TileImage" content="/favicon/mstile-144x144.png" />
           <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
           <meta name="theme-color" content="#ffffff" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
         </Head>
         <div className={theme + ' min-h-[100vh]'}>
           <Component {...pageProps}></Component>
