f21ef9b7e08fc7f420256b1ad7d551d4a1b81088
PMinn <40393799+PMinn@users.noreply.github.com>
2024-01-08 23:28:05 +0800

    Update icon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -26,6 +26,21 @@ export default function MyApp({ Component, pageProps }) {
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
           <title>會員禮計算機</title>
+
+
+
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="shortcut icon" href="/favicon/favicon.ico" />
+          <meta name="apple-mobile-web-app-title" content="會員禮計算機" />
+          <meta name="application-name" content="會員禮計算機" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="/favicon/mstile-144x144.png" />
+          <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <div className={theme + ' min-h-[100vh]'}>
           <Component {...pageProps}></Component>
